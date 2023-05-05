16ce20bd079babfa24372df758c9a50f0a49db70
Jeremy Bunting <187095+qbunt@users.noreply.github.com>
2023-05-05 10:42:04 -0400

    refactor on routes

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import '$styles/app.css'
+  import Footer from '$components/Footer.svelte'
+</script>
+
+<svelte:head>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.min.css" />
+</svelte:head>
+<main>
+  <slot />
+  <Footer name={import.meta.env.VITE_SITE_AUTHOR} />
+</main>
