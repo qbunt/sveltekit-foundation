79c99a4881ba32bd996e34fcf5be18904558a6a5
Jeremy Bunting <187095+qbunt@users.noreply.github.com>
2023-05-13 23:31:09 -0400

    adding siteconfig

--- src/lib/siteConfig.js
+++ src/lib/siteConfig.js
@@ -4,5 +4,12 @@ export const config = {
   siteDescription: import.meta.env.VITE_SITE_DESCRIPTION,
   siteAuthor: import.meta.env.VITE_SITE_AUTHOR,
   language: import.meta.env.VITE_SITE_LANGUAGE,
-  copyright: `©${new Date().getFullYear()} ${import.meta.env.VITE_SITE_AUTHOR}`
+  copyright: `©${new Date().getFullYear()} ${import.meta.env.VITE_SITE_AUTHOR}`,
+  headerNav: [
+    { name: "Blog", link: "/blog" },
+    { name: "About", link: "/about" },
+    { name: "Projects", link: "/projects" },
+    { name: "Contact", link: "/contact" },
+    { name: "Uses", link: "/posts/uses" }
+  ]
 }
